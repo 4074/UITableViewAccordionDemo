67cb8dbf4a028fad835c078469e675ee95150177
dengwenfeng <dwfn3398@corp.netease.com>
2016-07-02 20:51:39 +0800

    complete

---
+++ UITableViewAccordionDemo/UITableViewAccordionDemo/AccordionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccordionTableViewCell.swift
+//  UITableViewAccordionDemo
+//
+//  Created by wen on 16/7/2.
+//  Copyright © 2016年 wenfeng. All rights reserved.
+//
+
+import UIKit
+
+class AccordionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UITableViewAccordionDemo/UITableViewAccordionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewAccordionDemo
+//
+//  Created by wen on 16/7/2.
+//  Copyright © 2016年 wenfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
